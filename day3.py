b2513acfaf7a5ce5cd91386c40988eb461e0b841
Samuel Barata <samuel@barata.pt>
2023-12-03 14:46:46 +0000

    Refactor Day 3

--- day3.py
+++ day3.py
@@ -21,70 +21,17 @@ class Matrix():
     def __init__(self, bidimensional_array, gear):
         self.gear = gear
         self.bidimensional_array = bidimensional_array
-        """
-        467..114..
-        ...*......
-        ..35..633.
-        ......#...
-        617*......
-        .....+.58.
-        ..592.....
-        ......755.
-        ...$.*....
-        .664.598..
-        """
         self.gears = []
         self.next_to_symbol = []
-        self.next_to_number = []
         self.adjacent_numbers = []
         self.products = []
+        self.gear_ratios = 0
         self.analyse()
+        self.calculate_adjacent_numbers()
+        if self.gear:
+            self.find_gears()
+            self.calculate_gear_ratios()
 
-
-    def get_next_to_number(self, linha, coluna):
-        if self.bidimensional_array[linha][coluna] != '*':
-            return False
-        try:
-            if self.bidimensional_array[linha-1][coluna-1].isdigit():
-                return True
-        except:
-            pass
-        try:
-            if self.bidimensional_array[linha-1][coluna].isdigit():
-                return True
-        except:
-            pass
-        try:
-            if self.bidimensional_array[linha-1][coluna+1].isdigit():
-                return True
-        except:
-            pass
-        try:
-            if self.bidimensional_array[linha][coluna-1].isdigit():
-                return True
-        except:
-            pass
-        try:
-            if self.bidimensional_array[linha][coluna+1].isdigit():
-                return True
-        except:
-            pass
-        try:
-            if self.bidimensional_array[linha+1][coluna-1].isdigit():
-                return True
-        except:
-            pass
-        try:
-            if self.bidimensional_array[linha+1][coluna].isdigit():
-                return True
-        except:
-            pass
-        try:
-            if self.bidimensional_array[linha+1][coluna+1].isdigit():
-                return True
-        except:
-            pass
-        return False
 
     def is_symbol(self, char):
         if self.gear:
@@ -92,57 +39,27 @@ class Matrix():
         return not char.isdigit() and char != '.'
 
     def get_next_to_symbol(self, linha, coluna):
-        try:
-            if self.is_symbol(self.bidimensional_array[linha-1][coluna-1]):
-                return True
-        except:
-            pass
-        try:
-            if self.is_symbol(self.bidimensional_array[linha-1][coluna]):
-                return True
-        except:
-            pass
-        try:
-            if self.is_symbol(self.bidimensional_array[linha-1][coluna+1]):
-                return True
-        except:
-            pass
-        try:
-            if self.is_symbol(self.bidimensional_array[linha][coluna-1]):
-                return True
-        except:
-            pass
-        try:
-            if self.is_symbol(self.bidimensional_array[linha][coluna+1]):
-                return True
-        except:
-            pass
-        try:
-            if self.is_symbol(self.bidimensional_array[linha+1][coluna-1]):
-                return True
-        except:
-            pass
-        try:
-            if self.is_symbol(self.bidimensional_array[linha+1][coluna]):
-                return True
-        except:
-            pass
-        try:
-            if self.is_symbol(self.bidimensional_array[linha+1][coluna+1]):
-                return True
-        except:
-            pass
+        for li in range(linha-1, linha+2):
+            tmp = 0
+            for co in range(coluna-1, coluna+2):
+                if tmp > 0:
+                    tmp -= 1
+                    continue
+                if li >= len(self.bidimensional_array) or co >= len(self.bidimensional_array[li]):
+                    continue
+                if li == linha and co == coluna:
+                    continue
+                if self.is_symbol(self.bidimensional_array[li][co]):
+                    return True
         return False
 
     def analyse(self):
         for linha in range(len(self.bidimensional_array)):
             self.next_to_symbol.append([])
-            self.next_to_number.append([])
             for coluna in range(len(self.bidimensional_array[linha])):
-                self.next_to_number[-1].append(self.get_next_to_number(linha, coluna))
                 self.next_to_symbol[-1].append(self.get_next_to_symbol(linha, coluna))
 
-    def return_adjacent_numbers(self):
+    def calculate_adjacent_numbers(self):
         tmp = 0
         for linha in range(len(self.bidimensional_array)):
             for coluna in range(len(self.bidimensional_array[linha])):
@@ -188,35 +105,28 @@ class Matrix():
                     res.append(self.get_full_number(li, co))
                     while co+tmp < len(self.bidimensional_array[li]) and self.bidimensional_array[li][co+tmp].isdigit():
                         tmp += 1
-        if len(res) == 2:
-            return res
-        return None
+        return res
 
     def find_gears(self):
         for linha in range(len(self.bidimensional_array)):
             for coluna in range(len(self.bidimensional_array[linha])):
-                if self.next_to_number[linha][coluna]:
+                if self.bidimensional_array[linha][coluna] == '*':
                     numbers = self.get_numbers_arround(linha, coluna)
-                    if numbers is not None:
+                    if len(numbers) == 2:
                         self.gears.append(numbers)
 
+    def calculate_gear_ratios(self):
+        for gear in self.gears:
+            self.gear_ratios += gear[0]*gear[1]
 
 if __name__ == '__main__':
-    chal1 = 0
-    chal2 = 0
     data = []
     with open('day3.input', 'r') as f:
         for line in f.readlines():
             data.append(line.strip())
 
-    a = Matrix(data, True)
-    print(a.next_to_number)
+    chal1 = Matrix(data,False)
+    chal2 = Matrix(data,True)
 
-    a.return_adjacent_numbers()
-    a.find_gears()
-    for i in a.gears:
-        chal2 += i[0]*i[1]
-    print(a.gears)
-
-    print(f"Challenge 1: {sum(a.adjacent_numbers)}")
-    print(f"Challenge 2: {chal2}")
+    print(f"Challenge 1: {sum(chal1.adjacent_numbers)}")
+    print(f"Challenge 2: {chal2.gear_ratios}")
